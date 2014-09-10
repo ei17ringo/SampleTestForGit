6643a9b4778ab0dccce02be2e42447e05bb10079
ei17ringo <ei17ringo@gmail.com>
2014-09-10 14:49:08 +0800

    Initial Commit

---
+++ SampleTestForGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleTestForGit
+//
+//  Created by Eriko Ichinohe on 2014/09/10.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
